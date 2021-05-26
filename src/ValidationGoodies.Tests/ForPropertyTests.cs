2009e86e868a56b790f2d0ea01c7c3702a1df2d1
ming lu <ming.c.lu@gmail.com>
2021-05-26 13:08:55 -0400

    added test for more than one item.

--- src/ValidationGoodies.Tests/ForPropertyTests.cs
+++ src/ValidationGoodies.Tests/ForPropertyTests.cs
@@ -52,5 +52,45 @@ namespace ValidationGoodies.Tests
             result.Errors.Count.Should().Be(1);
             result.Errors.First().ErrorMessage.Should().Be("'Items[0].Name' must not be empty.");
         }
+
+        [Fact]
+        public async Task Cascade_validates_with_more_than_one_items()
+        {
+            RuleForEach(a => a.Items)
+                .ForProperty(a => a.Name, builder =>
+                {
+                    builder.Cascade().NotEmpty().Length(0, 10).Must((propName, propValue, parent, item, context) => { return false; }, "rule must failed, third errors.");
+                });
+            var order = new Order { Items = new[] { new Item(), new Item() } };
+
+            var result = await this.ValidateAsync(order);
+
+            result.Errors.Should().NotBeEmpty();
+            result.Errors.Count.Should().Be(6);
+            result.Errors[0].ErrorMessage.Should().Be("'Items[0].Name' must not be empty.");
+            result.Errors[1].ErrorMessage.Should().Be("'Items[0].Name' must be between 0 and 10 characters. You entered 0 characters.");
+            result.Errors[2].ErrorMessage.Should().Be("'Items[0].Name' rule must failed, third errors.");
+            result.Errors[3].ErrorMessage.Should().Be("'Items[1].Name' must not be empty.");
+            result.Errors[4].ErrorMessage.Should().Be("'Items[1].Name' must be between 0 and 10 characters. You entered 0 characters.");
+            result.Errors[5].ErrorMessage.Should().Be("'Items[1].Name' rule must failed, third errors.");
+        }
+
+        [Fact]
+        public async Task NoCascade_validates_with_more_than_one_items()
+        {
+            RuleForEach(a => a.Items)
+                .ForProperty(a => a.Name, builder =>
+                {
+                    builder.NotEmpty().Length(0, 10).Must((propName, propValue, parent, item, context) => { return false; }, "rule must failed, third errors.");
+                });
+            var order = new Order { Items = new[] { new Item(), new Item() } };
+
+            var result = await this.ValidateAsync(order);
+
+            result.Errors.Should().NotBeEmpty();
+            result.Errors.Count.Should().Be(2);
+            result.Errors[0].ErrorMessage.Should().Be("'Items[0].Name' must not be empty.");
+            result.Errors[1].ErrorMessage.Should().Be("'Items[1].Name' must not be empty.");
+        }
     }
 }
