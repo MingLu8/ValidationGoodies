acd7fb5134e31bbc456563824ba3d1c8d2bab81a
ming lu <ming.c.lu@gmail.com>
2021-05-27 14:02:31 -0400

    added error message overloads.

--- src/ValidationGoodies/IPropertyRules.cs
+++ src/ValidationGoodies/IPropertyRules.cs
@@ -13,17 +13,26 @@ namespace ValidationGoodies
         TElement InstanceToValidate { get; }
         T ParentInstanceToValidate { get; }
         ValidationContext<T> Context { get; }
-        PropertyRules<T, TElement, TPropertyType> Cascade();
-        PropertyRules<T, TElement, TPropertyType> NotEmpty();
-        PropertyRules<T, TElement, TPropertyType> Max(TPropertyType max);
-        PropertyRules<T, TElement, TPropertyType> Min(TPropertyType min);
-        PropertyRules<T, TElement, TPropertyType> Length(int min, int max);
-        PropertyRules<T, TElement, TPropertyType> Length(int exactValue);
-        PropertyRules<T, TElement, TPropertyType> Matches(string expression) => Matches(expression, "has invalid format value.");
-        PropertyRules<T, TElement, TPropertyType> Matches(string expression, string errorMessage);
-        PropertyRules<T, TElement, TPropertyType> Must(Func<bool> func) => Must(func, "is invalid.");
-        PropertyRules<T, TElement, TPropertyType> Must(Func<bool> func, string errorMessage);
-        Task<PropertyRules<T, TElement, TPropertyType>> MustAsync(Func<Task<bool>> func) => MustAsync(func, "is invalid.");
-        Task<PropertyRules<T, TElement, TPropertyType>> MustAsync(Func<Task<bool>> func, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> Cascade();
+        IPropertyRules<T, TElement, TPropertyType> NotEmpty() => NotEmpty("must not be empty.");
+        IPropertyRules<T, TElement, TPropertyType> NotEmpty(string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> Max(TPropertyType max) => Max(max, $"cannot be greater than {max}, You entered {PropertyValue}.");
+        IPropertyRules<T, TElement, TPropertyType> Max(TPropertyType max, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> Min(TPropertyType min) => Min(min, $"cannot be less than {min}, You entered {PropertyValue}.");
+        IPropertyRules<T, TElement, TPropertyType> Min(TPropertyType min, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> Length(int min, int max) => Length(min, max, $"must be between {min} and {max} characters. You entered {PropertyValue?.ToString().Length ?? 0} characters.");
+        IPropertyRules<T, TElement, TPropertyType> Length(int min, int max, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> Length(int exactValue) => Length(exactValue, $"must be exactly {exactValue} characters. You entered {PropertyValue?.ToString().Length ?? 0} characters.");
+        IPropertyRules<T, TElement, TPropertyType> Length(int exactValue, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> MaxLength(int max) => MaxLength(max, $"must not be more than {max} characters. You entered {PropertyValue?.ToString().Length ?? 0} characters.");
+        IPropertyRules<T, TElement, TPropertyType> MaxLength(int max, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> MinLength(int min) => MinLength(min, $"must not be less than {min} characters. You entered {PropertyValue?.ToString().Length ?? 0} characters.");
+        IPropertyRules<T, TElement, TPropertyType> MinLength(int min, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> Matches(string expression) => Matches(expression, "has invalid format value.");
+        IPropertyRules<T, TElement, TPropertyType> Matches(string expression, string errorMessage);
+        IPropertyRules<T, TElement, TPropertyType> Must(Func<bool> func) => Must(func, "is invalid.");
+        IPropertyRules<T, TElement, TPropertyType> Must(Func<bool> func, string errorMessage);
+        Task<IPropertyRules<T, TElement, TPropertyType>> MustAsync(Func<Task<bool>> func) => MustAsync(func, "is invalid.");
+        Task<IPropertyRules<T, TElement, TPropertyType>> MustAsync(Func<Task<bool>> func, string errorMessage);
     }
 }
--- src/ValidationGoodies/PropertyRules.cs
+++ src/ValidationGoodies/PropertyRules.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation;
+using FluentValidation.Validators;
 
 namespace ValidationGoodies
 {
@@ -28,93 +29,88 @@ namespace ValidationGoodies
             Context = context;
         }
 
-        public virtual PropertyRules<T, TElement, TPropertyType> Cascade()
+        public virtual IPropertyRules<T, TElement, TPropertyType> Cascade()
         {
             NoCascade = false;
             return this;
         }
 
-        public virtual PropertyRules<T, TElement, TPropertyType> NotEmpty()
+        public virtual IPropertyRules<T, TElement, TPropertyType> NotEmpty(string errorMessage)
         {
             if (NoCascade && Failed || NotEmptyInternal()) return this;
 
-            return AddFailure("must not be empty.");
+            return AddFailure(errorMessage);
         }
 
-        public virtual PropertyRules<T, TElement, TPropertyType> Max(TPropertyType max)
+        public virtual IPropertyRules<T, TElement, TPropertyType> Max(TPropertyType max, string errorMessage)
         {
             if (NoCascade && Failed) return this;
-            return PropertyValue?.CompareTo(max) <= 0 ? this : AddFailure($"cannot be greater than {max}, You entered {PropertyValue}.");
+            return PropertyValue?.CompareTo(max) <= 0 ? this : AddFailure(errorMessage);
         }
 
-        public virtual PropertyRules<T, TElement, TPropertyType> Min(TPropertyType min)
-        {
-            if (NoCascade && Failed) return this;
-
-            return PropertyValue?.CompareTo(min) >= 0 ? this : AddFailure($"cannot be less than {min}, You entered {PropertyValue}.");
-        }
-
-        public virtual PropertyRules<T, TElement, TPropertyType> Length(int exactValue)
+        public virtual IPropertyRules<T, TElement, TPropertyType> Min(TPropertyType min, string errorMessage)
         {
             if (NoCascade && Failed) return this;
 
-            var length = PropertyValue?.ToString().Length;
-            if (length != null && length == exactValue) return this;
-
-            return AddFailure($"must be {exactValue} characters. You entered {length ?? 0} characters.");
+            return PropertyValue?.CompareTo(min) >= 0 ? this : AddFailure(errorMessage);
         }
 
-        public virtual PropertyRules<T, TElement, TPropertyType> Length(int min, int max)
+        public virtual IPropertyRules<T, TElement, TPropertyType> MaxLength(int max, string errorMessage)
         {
-            if (NoCascade && Failed) return this;
+            if (NoCascade && Failed || MaxLengthInternal(max)) return this;
 
-            var length = PropertyValue?.ToString().Length;
-            if (length != null && length <= max && length >= min) return this;
-
-            return AddFailure($"must be between {min} and {max} characters. You entered {length ?? 0} characters.");
+            return AddFailure(errorMessage);
         }
 
-        public virtual PropertyRules<T, TElement, TPropertyType> Must(Func<bool> func, string errorMessage)
+        public virtual IPropertyRules<T, TElement, TPropertyType> MinLength(int min, string errorMessage)
+        {
+            if (NoCascade && Failed || MinLengthInternal(min)) return this;
+
+            return AddFailure(errorMessage);
+        }
+
+        public virtual IPropertyRules<T, TElement, TPropertyType> Length(int exactValue, string errorMessage)
+        {
+            if (NoCascade && Failed || LengthInternal(exactValue)) return this;
+            
+            return AddFailure(errorMessage);
+        }
+
+        public virtual IPropertyRules<T, TElement, TPropertyType> Length(int min, int max, string errorMessage)
+        {
+            if (NoCascade && Failed || LengthInternal(min, max)) return this;
+            
+            return AddFailure(errorMessage);
+        }
+
+        public virtual IPropertyRules<T, TElement, TPropertyType> Must(Func<bool> func, string errorMessage)
         {
             if (NoCascade && Failed || func()) return this;
 
             return AddFailure(errorMessage);
         }
 
-        public virtual PropertyRules<T, TElement, TPropertyType> Matches(string expression, string errorMessage)
+        public virtual IPropertyRules<T, TElement, TPropertyType> Matches(string expression, string errorMessage)
         {
-            if (PropertyValue != null && Regex.IsMatch(PropertyValue.ToString(), expression)) return this;
+            if (NoCascade && Failed || MatchesInternal(expression)) return this;
 
             return AddFailure(errorMessage);
         }
 
-        public virtual async Task<PropertyRules<T, TElement, TPropertyType>> MustAsync(Func<Task<bool>> func, string errorMessage)
+        public virtual async Task<IPropertyRules<T, TElement, TPropertyType>> MustAsync(Func<Task<bool>> func, string errorMessage)
         {
             if (NoCascade && Failed || await func()) return this;
 
             return AddFailure(errorMessage);
         }
 
-        protected bool NotEmptyInternal()
-        {
-            switch (PropertyValue)
-            {
-                case null:
-                case string s when string.IsNullOrWhiteSpace(s):
-                case ICollection { Count: 0 }:
-                case Array { Length: 0 } c:
-                case IEnumerable e when !e.Cast<object>().Any():
-                    return false;
-            }
-
-            if (Equals(PropertyValue, default(TPropertyType)))
-            {
-                return false;
-            }
-
-            return true;
-        }
-        protected virtual PropertyRules<T, TElement, TPropertyType> AddFailure(string errorMessage)
+        protected bool NotEmptyInternal() => new NotEmptyValidator<T, TPropertyType>().IsValid(Context, PropertyValue);
+        protected bool LengthInternal(int min, int max) => new LengthValidator<T>(min, max).IsValid(Context, PropertyValue?.ToString());
+        protected bool LengthInternal(int exact) => new ExactLengthValidator<T>(exact).IsValid(Context, PropertyValue?.ToString());
+        protected bool MaxLengthInternal(int max) => new MaximumLengthValidator<T>(max).IsValid(Context, PropertyValue?.ToString());
+        protected bool MinLengthInternal(int min) => new MinimumLengthValidator<T>(min).IsValid(Context, PropertyValue?.ToString());
+        protected bool MatchesInternal(string expression) => new RegularExpressionValidator<T>(expression).IsValid(Context, PropertyValue?.ToString());
+        protected virtual IPropertyRules<T, TElement, TPropertyType> AddFailure(string errorMessage)
         {
             Failed = true;
             Context.AddFailure(PropertyName, $"'{Context.PropertyName}.{PropertyName}' {errorMessage}");
